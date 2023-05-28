b993fcce524dbe58b22bd7c15b338a42f5b61e7f
Yarden Tal <yarden.tal@quatro-digital.com>
2023-05-28 08:14:37 +0300

    amend

--- lib/config/shadow.dart
+++ lib/config/shadow.dart
@@ -1,5 +1,3 @@
 import 'package:flutter/material.dart';
 
-applyShadow() {
-  return [const Shadow(color: Colors.black87, blurRadius: 20, offset: Offset(4, 6))];
-}
+applyShadow() => [const Shadow(color: Colors.black87, blurRadius: 20, offset: Offset(4, 6))];
