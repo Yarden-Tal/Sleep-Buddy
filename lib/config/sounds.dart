c7823b9ddf4832a48dcea607cba68a857b1dc31e
Yarden Tal <83368934+Yarden-Tal@users.noreply.github.com>
2023-09-11 11:04:27 +0300

    assets changes

--- lib/config/sounds.dart
+++ lib/config/sounds.dart
@@ -2,7 +2,7 @@
 
 class Sound {
   static const String rain = "audio/rain.mp3";
-  static const String whiteNoise = "audio/white-noise.mp3";
+  static const String whiteNoise = "audio/white-noise.flac";
   static const String waves = "audio/waves.mp3";
   static const String forest = "audio/forest.mp3";
 }
--- lib/utils/sound_utils.dart
+++ lib/utils/sound_utils.dart
@@ -1,6 +1,6 @@
 /// Capitalizes & removes file-name, file-type
 String formatSoundName(String sound) {
-  String cleanSoundName = sound.replaceAll("audio/", "").replaceAll(".mp3", "").replaceAll("-", " ");
+  String cleanSoundName = sound.replaceAll("audio/", "").replaceAll(".mp3", "").replaceAll(".flac", "").replaceAll("-", " ");
   String firstLetter = cleanSoundName[0].toUpperCase();
   String restOfWord = cleanSoundName.substring(1);
   String capitalizedSoundName = "$firstLetter$restOfWord";
